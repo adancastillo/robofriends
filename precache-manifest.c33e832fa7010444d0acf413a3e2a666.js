94f814a51063fb6042888a7cacfd96bfc8790f6f
Adan Castillo <adancastillo@tutanota.com>
2020-05-19 01:57:43 -0600

    Updates

---
+++ precache-manifest.c33e832fa7010444d0acf413a3e2a666.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25407d6b0f00688f69b8732bb077a4f",
+    "url": "/adancastillo/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/adancastillo/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4dad5bd843d593e7efb2",
+    "url": "/adancastillo/robofriends/static/css/main.26d9e5cc.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/adancastillo/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adancastillo/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dad5bd843d593e7efb2",
+    "url": "/adancastillo/robofriends/static/js/main.b164c619.chunk.js"
+  },
+  {
+    "revision": "dd89506aa19773438c25",
+    "url": "/adancastillo/robofriends/static/js/runtime-main.f6cca8e7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/adancastillo/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
